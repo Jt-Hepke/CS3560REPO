3afd112ca21536f18141c2e16e413afb72030faf
John Hepke <johnhepke@Johns-MacBook-Pro.local>
2026-02-28 14:35:40 -0500

    Start of python player

---
+++ programming101assignment/python/main.py
@@ -0,0 +1,13 @@
+#Python RPG GAME
+
+#player class
+class Player:
+    def __init__(self):
+        self.health = 100
+        self.maxHealth = 100
+
+#create the player
+player = Player()
+
+print("Player made")
+print("Health:", player.health) #read the health
